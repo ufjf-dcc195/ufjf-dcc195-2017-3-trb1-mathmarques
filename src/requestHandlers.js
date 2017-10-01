41f640d2c88953171a7423b5fc06bba30434dbce
Matheus Marques <matheusocmarques@gmail.com>
2017-10-01 11:49:33 -0300

    Added equation

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -134,6 +134,50 @@ function prime(request, response) {
     response.end();
 }
 
+// Equação
+function equation(request, response) {
+    var isPost = (request.method === 'POST');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(template.header);
+    response.write('<h2 class="text-center">#Equação</h2>');
+    response.write('<p class="text-center">Resolver a equação: <b>a</b>x<sup>2</sup>+<b>b</b>x+<b>c</b>=0</p>');
+    response.write('<form class="form-inline" method="POST">');
+    response.write('<div class="form-group"><label for="a">Valor A: </label> <input type="text" class="form-control" name="a" id="a" value="'+ (isPost? request.post.a : '') +'"></div>');
+    response.write(' <div class="form-group"><label for="b">Valor B: </label> <input type="text" class="form-control" name="b" id="b" value="'+ (isPost? request.post.b : '') +'"></div>');
+    response.write(' <div class="form-group"><label for="c">Valor C: </label> <input type="text" class="form-control" name="c" id="c" value="'+ (isPost? request.post.c : '') +'"></div>');
+    response.write(' <button type="submit" class="btn btn-default">Resolver</button></form><br/><br/>');
+
+    if(isPost){
+        var a = parseInt(request.post.a);
+        var b = parseInt(request.post.b);
+        var c = parseInt(request.post.c);
+        if(isNaN(a) || isNaN(b) || isNaN(c) || (a === 0 && b === 0))
+            response.write('<p class="text-center">Valores inválidos! a,b e c devem ser números inteiros e a ou b diferente de 0!</p>');
+        else {
+            var eq = '';
+            eq += (a === 0? '' : '<b>'+(a === 1? '': a)+'</b>x<sup>2</sup>');
+            eq += (b === 0? '' : (b < 0? ' - ': (a === 0? '' : ' + '))+ '<b>'+(b === 1? '': Math.abs(b))+'</b>x');
+            eq += (c === 0? '' : (c < 0? ' - ':' + ')+ '<b>'+Math.abs(c)+'</b> = 0');
+            response.write('<p class="text-center">Resolvendo a equação: ' + eq +'</p><br/>');
+
+            if(a === 0) {
+                response.write('<p class="text-center"><b>Solução: </b> x = '+ (-c)/b +'</p>');
+            } else {
+                var delta = (b*b) - (4*a*c);
+                if(delta < 0)
+                    response.write('<p class="text-center">Não existem raízes reais (delta < 0)</p>');
+                else if(delta === 0)
+                    response.write('<p class="text-center"><b>Solução: </b> raíz única(delta = 0) = '+ (-b)/(2*a) +'</p>');
+                else
+                    response.write('<p class="text-center"><b>Solução: </b> x\' = '+ ((-b)+Math.sqrt(delta))/(2*a) +' e x\' = '+ ((-b)-Math.sqrt(delta))/(2*a) +'</p>');
+            }
+        }
+
+    }
+    response.write(template.footer);
+    response.end();
+}
+
 function intervalo(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('<!DOCTYPE html><html><head></head><body>');
@@ -159,4 +203,5 @@ exports.index = index;
 exports.about = about;
 exports.random = random;
 exports.prime = prime;
+exports.equation = equation;
 exports.intervalo = intervalo;
